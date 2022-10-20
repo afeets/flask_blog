0525be99d3f45c7b0793c5824ce56b48eb577a73
Andy Feetenby <Andy.Feetenby@icr.co.th>
2022-10-20 15:45:16 +0700

    Setup SQLAlchemy connection to SQLite. User Routes now check authentication. Updated HTML Templates, conditional based on user logged in

--- Flask_Blog/app/User/views.py
+++ Flask_Blog/app/User/views.py
@@ -1,6 +1,8 @@
-from app import app
-from flask import Blueprint, render_template, redirect, url_for, flash
+from app import app, db, bcrypt
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask_login import login_user, logout_user, current_user, login_required
 from app.User.forms import LoginForm, RegistrationForm
+from app.models import User
 
 user_blueprint = Blueprint('User',
                                 __name__,
@@ -8,23 +10,59 @@ user_blueprint = Blueprint('User',
 
 @user_blueprint.route("/register", methods=["GET","POST"])
 def register():
+  if current_user.is_authenticated:
+    return redirect(url_for('home'))
   form = RegistrationForm()
 
   if form.validate_on_submit():
-    flash(f'Account created for { form.username.data }!', 'success')
-    return redirect(url_for('home')) 
+    
+    hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+    user = User(
+      username = form.username.data, 
+      email = form.email.data, 
+      password = hashed_password
+    )
+
+    # Add user to Database
+    db.session.add(user)
+    db.session.commit()
+
+    flash('Your account has been created. You can now log in.', 'success')
+    return redirect(url_for('User.login')) 
 
   return render_template('register.html', title='Register', form=form)
 
 
 @user_blueprint.route("/login", methods=["GET","POST"])
 def login():
+  if current_user.is_authenticated:
+    return redirect(url_for('home'))
   form = LoginForm()
   if form.validate_on_submit():
     # attempt login
-    if form.email.data == 'admin@gmail.com' and form.password.data == 'password':
-      flash('You have been logged in!', 'success')
-      return redirect(url_for('home'))
+    # if form.email.data == 'admin@gmail.com' and form.password.data == 'password':
+    #  flash('You have been logged in!', 'success')
+    #  return redirect(url_for('home'))
+    
+    user = User.query.filter_by(email=form.email.data).first()
+    if user and bcrypt.check_password_hash(user.password, form.password.data):
+      login_user(user, remember=form.remember.data)
+
+      # if previously redirected to the login page, 
+      # look at next_page query string to define where to go now after completing login
+      next_page = request.args.get('next') 
+      return redirect(next_page) if next_page else redirect(url_for('home'))
     else:
-      flash('Login Unsuccessful. Please check Username and Password', 'danger')
+      flash('Login Unsuccessful. Please check Email and Password', 'danger')
   return render_template('login.html', title='Login', form=form)
+
+@user_blueprint.route("/logout")
+def logout():
+  logout_user()
+  return redirect(url_for('home'))
+
+
+@user_blueprint.route("/account")
+@login_required
+def account():
+  return render_template('account.html', title='Account')
--- Flask_Blog/app/__init__.py
+++ Flask_Blog/app/__init__.py
@@ -1,8 +1,27 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'd119af253a2062ec517bd2c4887423fb'
+
+# Configure SQLite DB
+db_path = os.path.join(os.path.dirname(__file__), 'site.db')
+db_uri = 'sqlite:///{}'.format(db_path)
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'User.login'
+login_manager.login_message_category = 'info'
 
 from app import views
 
