8026a24086038f57f7af4bbfb18a881f89d9ee75
Andy Feetenby <Andy.Feetenby@icr.co.th>
2022-10-21 12:51:52 +0700

    Added Update functionality to Post view

--- Flask_Blog/app/Post/views.py
+++ Flask_Blog/app/Post/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask import Blueprint, render_template, request, redirect, url_for, flash, abort
 from flask_login import login_required, current_user
 from app.Post.forms import PostForm
 from app import app, db
@@ -20,4 +20,36 @@ def new():
     db.session.commit()
     flash('Your post has been created', 'success')
     return redirect(url_for('home'))
-  return render_template('new.html', title='New Post', form=form)
+  return render_template('new.html', title='New Post', legend='New Post', form=form)
+
+
+@post_blueprint.route("/<int:post_id>")
+def get(post_id):
+  post = Post.query.get_or_404(post_id)
+  return render_template('post.html',title=post.title, post=post)
+
+@post_blueprint.route("/update/<int:post_id>", methods=["GET","POST"])
+@login_required
+def update(post_id):
+  post = Post.query.get_or_404(post_id)
+
+  # check author of post to check access
+  if post.author != current_user:
+    abort(403)
+
+  form = PostForm()
+
+  # if POST request
+  if form.validate_on_submit():
+    post.title      = form.title.data
+    post.content    = form.content.data
+
+    db.session.commit()
+    flash('Your Post has been updated', 'success')
+    return redirect(url_for('Post.get', post_id=post.id))
+
+  elif request.method == "GET":
+    form.title.data = post.title
+    form.content.data = post.content
+
+  return render_template('new.html', title='Update Post', legend='Update Post', form=form)
