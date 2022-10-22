1d1924e485e63fff7d52f41243f0babf3f32ccd2
Andy Feetenby <andyfeetenby@gmail.com>
2022-10-22 11:29:24 +0700

    Added Pagination of posts on homepage

--- Flask_Blog/app/views.py
+++ Flask_Blog/app/views.py
@@ -2,11 +2,13 @@ from app import app
 from flask import render_template, request, url_for
 from app.models import Post
 
-
+# url home/?page=<int>
 @app.route("/")
 @app.route("/home")
 def home():
-  posts = Post.query.all()
+  page = request.args.get('page', 1, type=int)
+  # paginate posts in descending time order
+  posts = Post.query.order_by(Post.date_posted.desc()).paginate(per_page=3, page = page)
   return render_template("public/home.html", posts = posts)
 
 @app.route("/about")
